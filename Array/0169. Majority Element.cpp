2ea9d7bc6a80412ac0f594d0283347c30a1acedb
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-02-18 00:52:36 +0530

    Create 0169. Majority Element.cpp

---
+++ Array/0169. Majority Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+            
+        }
+        for(auto x:m){
+            if(x.second>(nums.size()/2)){
+                return x.first;
+            }
+        }
+        return 0;
+    }
+};
