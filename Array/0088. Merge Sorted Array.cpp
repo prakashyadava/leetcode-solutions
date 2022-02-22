8b1ee4dc6e55eacf4f3f886dcf84d85f64d3f343
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-02-22 23:47:09 +0530

    Update 0088. Merge Sorted Array.cpp

--- Array/0088. Merge Sorted Array.cpp
+++ Array/0088. Merge Sorted Array.cpp
@@ -15,3 +15,33 @@ public:
         nums1 = arr;
     }
 };
+
+o(m+n)
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> arr;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                arr.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                arr.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<m){
+            arr.push_back(nums1[i]);
+                i++;
+        }
+        while(j<n){
+            arr.push_back(nums2[j]);
+                j++;
+        }
+        
+        nums1 = arr;
+    }
+};
