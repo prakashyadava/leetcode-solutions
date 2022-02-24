16ef1186458c44e58c17b47950db2dfd88342cd5
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-02-24 23:53:41 +0530

    Create 0448. Find All Numbers Disappeared in an Array.cpp

---
+++ Array/0448. Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        // set<int> s;
+        // int n = nums.size();
+        // for(auto x:nums){
+        //     s.insert(x);
+        // }
+        // nums = vector(s.begin(),s.end());
+        sort(nums.begin(),nums.end());
+        vector<int> res;
+        int temp =1;
+        
+    }
+};
