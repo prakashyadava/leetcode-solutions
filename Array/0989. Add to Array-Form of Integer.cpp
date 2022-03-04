6149d1888fd1adf7bf03aa935a9641a1b6708152
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-03-04 23:34:07 +0530

    Create 0989. Add to Array-Form of Integer.cpp

---
+++ Array/0989. Add to Array-Form of Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int sum = 0;
+        for(int i=0;i<num.size();i++){
+            sum = sum*10 + num[i];
+            
+        }
+        sum = sum + k;
+        vector<int> arr;
+        while(sum>0){
+            int tmp = sum%10;
+            sum = sum/10;
+            arr.push_back(tmp);
+        }
+        reverse(arr.begin(), arr.end());
+        return arr;
+    }
+};
