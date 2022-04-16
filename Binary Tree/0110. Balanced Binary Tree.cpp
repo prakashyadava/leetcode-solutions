7e15cc2917dfb225f6146a189f09d0d475bb666d
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-04-16 22:41:49 +0530

    Update 0110. Balanced Binary Tree.cpp

--- Binary Tree/0110. Balanced Binary Tree.cpp
+++ Binary Tree/0110. Balanced Binary Tree.cpp
@@ -38,3 +38,40 @@ public:
         return true;
     }
 };
+//O(n^2)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool chkBalanced(TreeNode* root, int*height){
+        if(root!=NULL){
+            int lh = 0,rh=0;
+            if(!chkBalanced(root->left,&lh)){
+                return false;
+            }
+            if(!chkBalanced(root->right,&rh)){
+                return false;
+            }
+            *height = max(lh,rh)+1;
+            if(abs(lh-rh)<=1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isBalanced(TreeNode* root) {
+        int height = 0;
+        return chkBalanced(root,&height);
+    }
+};
