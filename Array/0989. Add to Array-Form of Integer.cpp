97741c5a818f5826b5d98fe8186a0bdd7df5adec
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-06-14 12:31:31 +0530

    Update 0989. Add to Array-Form of Integer.cpp

--- Array/0989. Add to Array-Form of Integer.cpp
+++ Array/0989. Add to Array-Form of Integer.cpp
@@ -18,3 +18,33 @@ public:
         return arr;
     }
 };
+// o(n)
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& nums, int k) {
+        reverse(nums.begin(),nums.end());
+        int n = nums.size();
+        int carry = 0;
+        for(int i=0;i<n;i++){
+            int rem = k%10;
+            int temp = nums[i] + rem + carry;
+            if(temp>9){
+                carry = temp/10;
+                nums[i] = temp%10;
+                k = k/10;
+            }
+            else{
+                nums[i]+= rem + carry;
+                carry =0;
+                k=k/10;
+            }
+        }
+        k +=carry;
+        while(k!=0){
+            nums.push_back(k%10);
+            k = k/10;
+        }
+        reverse(nums.begin(),nums.end());
+        return nums;
+    }
+};
