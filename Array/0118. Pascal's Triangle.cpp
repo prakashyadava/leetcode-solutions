2b2d095848915b7742d47bef17715fd555ce4fcb
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-06-15 12:51:08 +0530

    Create 0118. Pascal's Triangle.cpp

---
+++ Array/0118. Pascal's Triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>>ans(numRows);
+        for(int i=0;i<numRows;i++){
+            ans[i].resize(i+1); // if you don't assign the size then it will throw an error
+            ans[i][0]=1;
+            ans[i][i]=1;
+            
+            for(int j=1;j<i;j++){
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+            }
+        }
+        return ans;
+    }
+};
