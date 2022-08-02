27950b4193900abd39b2068fae1ef5ff20641659
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-08-03 03:01:21 +0530

    Create 0053. Maximum Subarray.cpp

---
+++ Sliding Window/0053. Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int mx = nums[0];
+        int currSum = 0;
+        for(int i=0;i<nums.size();i++){
+            currSum += nums[i];
+            mx = max(mx,currSum);
+            currSum = max(0,currSum);
+        }
+        return mx;
+    }
+};
